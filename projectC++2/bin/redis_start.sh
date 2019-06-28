a71f526e1ea7a4219b020eeec9e5555f2724077a
Devilsider <784371251@qq.com>
2019-06-28 15:41:31 +0800

    添加redis缓存，摘要生成还未完成

--- projectC++2/bin/redis_start.sh
+++ projectC++2/bin/redis_start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/home/ubuntu/downloads/redis-5.0.5/src/redis-server /home/ubuntu/downloads/redis-5.0.5/redis.conf
+/home/ubuntu/downloads/redis-5.0.5/src/redis-server ~/projectC++2/conf/redis.conf
